99578bc4f6f50a482f41bc6cf71743455270397b
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-01-09 12:58:50 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 import sys
 import os
 import zlib
+import hashlib
 
 def handle_cat_file(args):
     print(read_file(args.sha), end="")
@@ -17,6 +18,16 @@ def read_file(sha):
             raise Exception(f"Malformed object {sha}: File size mismatch")
         return raw[content_start + 1:].decode()
 
+def create_object(content, format="blob"):
+    full_content = f"{format} {len(content)}\x00".encode() + content
+    sha = hashlib.sha1(full_content).hexdigest()
+    path = f".git/objects/{sha[:2]}/{sha[2:]}"
+    if os.path.exists(path):
+        return sha
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, "wb") as f:
+        f.write(zlib.compress(full_content))
+    return sha
 
 def main():
     command = sys.argv[1]
@@ -29,6 +40,10 @@ def main():
         print("Initialized git directory")
     elif command == "cat-file" and sys.argv[2] == "-p":
         print(read_file(sys.argv[3]), end="")
+    elif command == "hash-object":
+        with open(sys.argv[2], "r") as f:
+            content = f.read()
+        print(create_object(content))
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
