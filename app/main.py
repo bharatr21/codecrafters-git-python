93e98e2db05aba64229ef5688ac20717cba74eee
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2025-01-09 12:55:08 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
 from argparse import ArgumentParser
+import sys
 import os
+import zlib
+
+def handle_cat_file(args):
+    print(read_file(args.sha), end="")
+
+def read_file(sha):
+    with open(f".git/objects/{sha[:2]}/{sha[2:]}", "rb") as f:
+        raw = zlib.decompress(f.read())
+        start = raw.find(b" ")
+        format = raw[:start].decode()
+        content_start = raw.find(b"\x00")
+        file_size = int(raw[start:content_start].decode())
+        if file_size != len(raw[content_start + 1:]):
+            raise Exception(f"Malformed object {sha}: File size mismatch")
+        return raw[content_start + 1:].decode()
 
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument("command", help="The command to run")
-    args = parser.parse_args()
-    command = args.command
+    command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
         os.mkdir(".git/objects")
@@ -14,6 +27,8 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif command == "cat-file" and sys.argv[2] == "-p":
+        print(read_file(sys.argv[3]), end="")
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
